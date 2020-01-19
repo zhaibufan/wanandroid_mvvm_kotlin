80c7ecad9cbc25c0f034e1cf54ed24651afa4386
zhaixiaofan <zhaixiaofan@beyondsoft.com>
2020-01-19 18:14:54 +0800

    使用源集单独编译mvvm模块

---
+++ mvvm/src/main/java/com/beyondsoft/mvvm/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.beyondsoft.mvvm
+
+import com.beyondsoft.mvvm.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun getLayoutResId(): Int = R.layout.activity_main
+
+}
