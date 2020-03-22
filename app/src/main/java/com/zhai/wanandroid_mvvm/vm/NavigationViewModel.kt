d6ad94f2e609685a5d7e111dec4d0cd60e78ed4e
zhaixiaofan <842154133@qq.com>
2020-03-22 10:26:16 +0800

    test git pull

---
+++ app/src/main/java/com/zhai/wanandroid_mvvm/vm/NavigationViewModel.kt
@@ -0,0 +1,11 @@
+package com.zhai.wanandroid_mvvm.vm
+
+import com.beyondsoft.mvvm.base.BaseViewModel
+
+/**
+ * @author zhaixiaofan
+ * @date 2020-03-22 10:24
+ */
+class NavigationViewModel : BaseViewModel() {
+
+}
