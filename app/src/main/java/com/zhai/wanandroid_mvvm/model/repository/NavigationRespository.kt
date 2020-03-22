6d6818625a4bf2283dfd781153ecb3b25b78a351
zhaixiaofan <842154133@qq.com>
2020-03-22 10:21:41 +0800

    导航页面

---
+++ app/src/main/java/com/zhai/wanandroid_mvvm/model/repository/NavigationRespository.kt
@@ -0,0 +1,11 @@
+package com.zhai.wanandroid_mvvm.model.repository
+
+import com.beyondsoft.mvvm.base.BaseRepository
+
+/**
+ * @author zhaixiaofan
+ * @date 2020-03-22 10:20
+ */
+class NavigationRespository : BaseRepository() {
+
+}
