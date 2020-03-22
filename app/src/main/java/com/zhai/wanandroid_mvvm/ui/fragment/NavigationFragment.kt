125d5c0117a3e36c3314a1fbd098c42e4e5b84bb
zhaixiaofan <842154133@qq.com>
2020-03-22 10:39:06 +0800

    test git rebase

--- app/src/main/java/com/zhai/wanandroid_mvvm/ui/fragment/NavigationFragment.kt
+++ app/src/main/java/com/zhai/wanandroid_mvvm/ui/fragment/NavigationFragment.kt
@@ -3,10 +3,11 @@ package com.zhai.wanandroid_mvvm.ui.fragment
 import com.zhai.wanandroid_mvvm.R
 import com.beyondsoft.mvvm.base.BaseVMFragment
 import com.zhai.wanandroid_mvvm.vm.HomeViewModel
+import com.zhai.wanandroid_mvvm.vm.NavigationViewModel
 
-class NavigationFragment : BaseVMFragment<HomeViewModel>() {
+class NavigationFragment : BaseVMFragment<NavigationViewModel>() {
 
-    override fun providerVMClass(): Class<HomeViewModel>? = HomeViewModel::class.java
+    override fun providerVMClass(): Class<NavigationViewModel>? = NavigationViewModel::class.java
 
     override fun getLayoutResId(): Int = R.layout.fragment_navigation
 
